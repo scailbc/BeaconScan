5b996dd86191ddd83b5bbbd5c22b890b5f94d69c
Thierry <thierry.fornetti@consoft.it>
2017-02-24 14:33:28 +0100

    Init Beacon Ranging with Estimote

--- app/src/main/java/it/polito/s208912/beaconscan/BeconScanMainActivity.java
+++ app/src/main/java/it/polito/s208912/beaconscan/BeconScanMainActivity.java
@@ -2,12 +2,60 @@ package it.polito.s208912.beaconscan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.SystemRequirementsChecker;
+
+import java.util.List;
+import java.util.UUID;
 
 public class BeconScanMainActivity extends AppCompatActivity {
+
+    private BeaconManager bm;
+    private Region region;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_becon_scan_main);
+
+        this.bm = new BeaconManager(getApplicationContext());
+        this.region = new Region("ranged region",
+                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+
+        this.bm.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                bm.startRanging(region);
+            }
+        });
+
+        this.bm.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                if (!list.isEmpty()) {
+                    Beacon nearestBeacon = list.get(0);
+                    // TODO: update the UI here
+                    Log.d("Airport", "Nearest beacon: " + nearestBeacon.toString());
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        this.bm.stopRanging(this.region);
+        this.bm.disconnect();
+        super.onDestroy();
     }
 }
